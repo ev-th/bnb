9a2c1b6ec89123aef485dc374df61cff15839394
Julian Festing <julfesting@gmail.com>
2023-05-15 11:35:00 +0100

    email change

--- spec/seeds.sql
+++ spec/seeds.sql
@@ -1,7 +1,7 @@
 TRUNCATE TABLE users, listings, bookings RESTART IDENTITY;
 
-INSERT INTO users (email, password) VALUES ('julian@gmail.com', 'asdasd');
-INSERT INTO users (email, password) VALUES ('andrea@gmail.com', 'dsadsa');
+INSERT INTO users (email, password) VALUES ('julian@example.com', 'asdasd');
+INSERT INTO users (email, password) VALUES ('andrea@example.com', 'dsadsa');
 
 INSERT INTO listings (name, price, description, start_date, end_date, user_id) VALUES ('listing_1', '1,000.00', 'sunny place', '2023-04-08', '2023-05-09', '1');
 INSERT INTO listings (name, price, description, start_date, end_date, user_id) VALUES ('listing_2', '1,500.00', 'city penthouse', '2024-05-03', '2024-06-23', '2');
