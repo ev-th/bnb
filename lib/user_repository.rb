d6ffd67a6a5094be2a676d6bae8375d9b31229e9
Evan Thomas <evanthomas0292@gmail.com>
2023-05-15 15:05:35 +0100

    Write UserRepository#all

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -1,2 +1,18 @@
+require 'database_connection'
+require 'user'
+
 class UserRepository
+  def all
+    sql = 'SELECT id, email, password FROM users;'
+    result_set = DatabaseConnection.exec_params(sql, [])
+    users = []
+    result_set.each do |record|
+      user = User.new
+      user.id = record['id']
+      user.email = record['email']
+      user.password = record['password']
+      users << user
+    end
+    return users
+  end
 end
--- spec/user_repository_spec.rb
+++ spec/user_repository_spec.rb
@@ -1,6 +1,16 @@
 require 'user_repository'
 
+def reset_users_table
+  seed_sql = File.read('spec/seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'bnb_database_test' })
+  connection.exec(seed_sql)
+end
+
 RSpec.describe UserRepository do
+  before(:each) do 
+    reset_users_table
+  end
+  
   describe '#all' do
     it 'returns a list of all users' do
       repo = UserRepository.new
