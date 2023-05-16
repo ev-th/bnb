fd380bdcf4460a6e69fa3c1a1eda5c0d7e0b01cd
Cameron Murison <cmuris4@gmail.com>
2023-05-16 11:56:38 +0100

    buggy code needing fix for it to work

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/listing_repository'
+require_relative 'lib/database_connection'
+
+DatabaseConnection.connect('bnb_database_test')
 
 class Application < Sinatra::Base
   configure :development do
@@ -9,4 +13,25 @@ class Application < Sinatra::Base
   get '/' do
     return erb(:index)
   end
+  
+  get '/listings/:id' do
+    repo = ListingRepository.new
+
+    @listing_id = repo.find(params[:id])
+
+    return erb(:listings_id)
+  end
 end
+
+    # repo = AlbumRepository.new
+    # artist_repo = ArtistRepository.new
+
+    # @album = repo.find(params[:id])
+    # @artist = artist_repo.find(@album.artist_id)
+
+    # return erb(:album)
+
+# Returns an html view with a calendar available dates for a listing. 
+# User can choose on a date and click ‘request to book’ button 
+# to create a booking request on the database table.
+# dates for confirmed bookings shouldn’t be selectable.
---
+++ spec/integration/bookings_system_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+require 'json'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  # Write your integration tests below.
+  # If you want to split your integration tests
+  # accross multiple RSpec files (for example, have
+  # one test suite for each set of related features),
+  # you can duplicate this test file to create a new one.
+
+  context 'get/listings/:id' do
+    it "gets a listing based off of it's id" do
+      response = get('/listings/1')
+      expect(response.status).to eq(200)
+      expect(response.body).to include ('sunny place')
+      expect(response.body).to include ('<input type="date" id="date" min="2023-04-08" max="2024-06-23">')
+    end
+  end
+end
