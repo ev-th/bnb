1e8e4b95544efac1e2b766574e55ff9d5c5ca615
Evan Thomas <evanthomas0292@gmail.com>
2023-05-17 22:14:22 +0100

    Fix failing test due to change in html

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -24,13 +24,13 @@ describe Application do
 
       expect(response.status).to eq(200)
       expect(response.body).to include('<form method="POST" action="/signup">')
-      expect(response.body).to include('<input type="email" name="email">')
-      expect(response.body).to include('<input type="password" name="password">')
+      expect(response.body).to include('<input type="email" name="email" placeholder="email">')
+      expect(response.body).to include('<input type="password" name="password" placeholder="password">')
       expect(response.body).to include('<input type="submit">')
 
       expect(response.body).to include('<form method="POST" action="/login">')
-      expect(response.body).to include('<input type="email" name="email">')
-      expect(response.body).to include('<input type="password" name="password">')
+      expect(response.body).to include('<input type="email" name="email" placeholder="email">')
+      expect(response.body).to include('<input type="password" name="password" placeholder="password">')
       expect(response.body).to include('<input type="submit">')
     end
 
